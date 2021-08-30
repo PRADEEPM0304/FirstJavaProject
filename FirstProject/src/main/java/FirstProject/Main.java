d450a6bbdeed87bfb652305efc3d812c85fc8ed6
PRADEEPM0304 <PRADEEPM0304@GMAIL.COM>
2021-08-30 16:44:15 -0500

    Updated the Main

---
+++ FirstProject/src/main/java/FirstProject/Main.java
@@ -0,0 +1,16 @@
+package FirstProject;
+
+/**
+ *
+ * @author S545157
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
